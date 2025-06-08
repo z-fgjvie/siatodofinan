3e0eab17d8f006840e8f4b4ea411f2376597ea6c
Emerson <emecasti65@gmail.com>
2025-06-07 22:11:00 -0500

    zk

--- app/mi-cuenta/empresa/page.jsx
+++ app/mi-cuenta/empresa/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import HeaderCuenta from "../components/header-cuenta";
 import HeroCuenta from "../components/hero-cuenta";
 import MainCuenta from "../components/main-cuenta";
