98cb13c63fba53731c24b0d1c44b3aa7812bba6c
Emerson <emecasti65@gmail.com>
2025-06-18 21:15:36 -0500

    loadvv

--- app/mi-cuenta/components/loading-confirma.jsx
+++ app/mi-cuenta/components/loading-confirma.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function LoadingConfirma() {
   return (
-    <div className="flex justify-center items-center h-[15rem]">
+    <div className="flex justify-center items-center h-[15rem] mx-auto">
       {/* Spinner con logo */}
-      <div className="relative flex items-center justify-center w-22 h-22">
+      <div className="relative flex items-center justify-center w-22 h-22 mx-auto">
         <span className="absolute inline-flex h-full w-full animate-spin rounded-full border-4 border-t-[#09ce89] border-b-transparent border-l-transparent border-r-transparent" />
         <Image
           src="https://res.cloudinary.com/dauhuy8u6/image/upload/f_auto,q_auto/v1749930495/icon-financiera_pqpbmr.png"
